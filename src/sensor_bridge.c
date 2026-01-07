fb9ee725b7c097be54459104775bdb3ac1a8115f
guilhermsts <gui.santos@ua.pt>
2026-01-07 19:33:09 +0000

    sensor_bridge

--- src/sensor_bridge.c
+++ src/sensor_bridge.c
@@ -89,9 +89,15 @@ EXPORT SensorData get_sensor_readings(int channel, int sensivity) {
     veml3328_norm_rgb_t norm = veml3328_norm_colour(&raw_data, &cfg);
     out.R = rgb_255(norm.red);
     out.G = rgb_255(norm.green);
-    out.B = rgb_255(norm.blue);
+    out.B = rgb_255(norm.blue)  ;
     out.Intensity = norm.irradiance_uW_per_cm2;
     out.Wavelength = norm.wavelength;
+
+    fprintf(stderr,
+        "DBG bridge: raw C=%u R=%u G=%u B=%u | irr=%.3f wl=%.1f\n",
+        raw_data.clear, raw_data.red, raw_data.green, raw_data.blue,
+        norm.irradiance_uW_per_cm2, norm.wavelength);
+
     
     (void)tca_disable_all(i2c_fd, TCA9548A_ADDR);
     i2c_close_bus(i2c_fd);
